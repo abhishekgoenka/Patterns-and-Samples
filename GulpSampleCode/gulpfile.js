7ea0305d15fea50b8c607cf19b79a5470a3b4c2f
Goenka <Abhishek.Goenka@allscripts.com>
2017-01-06 09:08:06 +0530

    Added gulp-imagemin task

--- GulpSampleCode/gulpfile.js
+++ GulpSampleCode/gulpfile.js
@@ -5,7 +5,14 @@ var uglify = require('gulp-uglify');
 var myth = require('gulp-myth');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
+var imagemin = require('gulp-imagemin');
+//var clean = require('gulp-clean');
 
+// delete dist folder
+// todo : not working
+//gulp.task('clean', function() {
+//    clean('dist');
+//});
 // Process Styles
 gulp.task('styles', function() {
     return gulp.src('css/*.css')
@@ -24,11 +31,20 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('dist/'));
 });
 
+// Process Images
+gulp.task('images', function() {
+    return gulp.src('img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
+
+// Build
+gulp.task('build', ['styles', 'scripts', 'images']);
+
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('css/*.css', 'styles');
-    gulp.watch('js/*.js', 'scripts');
+    gulp.watch(['css/*.css', 'js/*.js', 'img/*.'], ['build']);
 });
 
 // Default Task
-//gulp.task('default', gulp.parallel('styles', 'scripts', 'watch'));
+gulp.task('default', ['styles', 'scripts', 'images', 'watch']);
