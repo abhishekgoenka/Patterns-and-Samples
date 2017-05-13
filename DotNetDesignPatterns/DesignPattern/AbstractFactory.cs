de693432570f0df9cce72c73181f1104b56be5c2
Abhishek <govindkaran@gmail.com>
2017-05-13 18:09:45 +0530

    Added AbstractFactory pattern

---
+++ DotNetDesignPatterns/DesignPattern/AbstractFactory.cs
@@ -0,0 +1,76 @@
+﻿using System;
+    public class AbstractFactory
+    {
+        public enum UISkillSet {  DotNet, Java };
+        public enum DBSkillSet {  MSSQL, MySQL };
+
+        /// <summary>
+        /// Abstract Factory : 
+        /// Provide an interface for creating families of related or dependent objects without 
+        /// specifying their concrete classes
+        /// </summary>
+        /// <param name="args"></param>
+        void Main(string[] args)
+        {
+            IBaseFactory baseFactory = new ProjectA(UISkillSet.DotNet, DBSkillSet.MSSQL);
+            Console.WriteLine(baseFactory.GetUITeam().GetTeam());
+
+            Console.ReadKey();
+        }
+
+        public interface IBaseFactory
+        {
+            ITeamFactory GetUITeam();
+            ITeamFactory GetDBTeam();
+        }
+
+        public interface ITeamFactory
+        {
+            String GetTeam();
+        }
+
+        public class ProjectA : IBaseFactory
+        {
+            private readonly UISkillSet UISkillSet;
+            private readonly DBSkillSet DBSkillSet;
+
+            public ProjectA(UISkillSet UISkillSet, DBSkillSet DBSkillSet)
+            {
+                this.UISkillSet = UISkillSet;
+                this.DBSkillSet = DBSkillSet;
+            }
+            public ITeamFactory GetUITeam()
+            {
+                switch (UISkillSet)
+                {
+                    case UISkillSet.DotNet:
+                        return new DotNetTeam();
+                    case UISkillSet.Java:
+                        return new JavaTeam();
+                    default:
+                        throw new IndexOutOfRangeException();
+                }
+            }
+
+            public ITeamFactory GetDBTeam()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class DotNetTeam : ITeamFactory
+        {
+            public string GetTeam()
+            {
+                return "DotNet Team";
+            }
+        }
+
+        public class JavaTeam : ITeamFactory
+        {
+            public string GetTeam()
+            {
+                return "Java Team";
+            }
+        }
+    }
